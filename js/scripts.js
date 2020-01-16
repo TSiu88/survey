7515224aab386d602dd3ecbf6dc41633b570e298
Tiffany Siu <tsiu88@gmail.com>
2020-01-15 18:18:28 -0800

    add survey options and output

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,26 @@
 $(document).ready(function(){
-  $("#survey").submit(function(event){
+  $("#survey").submit(function(){
+
+    var nameInput = $("#name").val();
+    var ageInput = $("#age").val();
+    var drinkInput = $("#beverage").val();
+    var iceCreamInput = $("input:radio[name=flavor]:checked").val();
+    var colorInput = $("#color").val();
+    var dateInput = $("#date").val();
+    var gameInput = $("#game").val();
+
+    $(".name").append(nameInput);
+    $(".age").append(ageInput);
+    $(".beverage").append(drinkInput);
+    $(".iceCream").append(iceCreamInput);
+    $(".color").append(colorInput);
+    $(".date").append(dateInput);
+    $(".game").append(gameInput);
+
+    $(".submitMessage").slideDown();
 
     event.preventDefault();
+
+    $(".btn").attr("disabled", true);
   });
 });
